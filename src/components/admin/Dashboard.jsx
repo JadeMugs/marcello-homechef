727090d4a50fad20980bd2da48d173bde72b4a86
JadeMugs <giada.boccali@gmail.com>
2021-02-20 22:24:44 +0100

    some fixes

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import * as Material from '@material-ui/core/';
 
 export default function Dashboard() {
-  return <Material.Button content="ciao" />;
+  return <Material.Button>ciao</Material.Button>;
 }
