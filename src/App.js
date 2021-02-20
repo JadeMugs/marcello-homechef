6a02fdbdfea94426f833266c92fe51c7f471781d
JadeMugs <giada.boccali@gmail.com>
2021-02-20 22:15:21 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import Home from './components/Home';
+import { Route, Router, Switch } from 'react-router-dom';
 import './index.scss';
 import './style/home.scss';
 import './style/components.scss';
+import Home from './components/Home';
+import Login from './components/admin/Login';
+import Dashboard from './components/admin/Dashboard';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
   );
 }
 
